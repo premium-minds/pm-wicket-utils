e1144b2a5c18ff7219f877d879bf0ad107b09516
cfraga <carlos.fraga@premium-minds.com>
2013-10-23 15:48:11 +0100

    fix to view when model object is null and added button column align

--- src/main/java/com/premiumminds/webapp/wicket/bootstrap/crudifier/view/CrudifierView.java
+++ src/main/java/com/premiumminds/webapp/wicket/bootstrap/crudifier/view/CrudifierView.java
@@ -21,65 +21,68 @@ import com.premiumminds.webapp.wicket.bootstrap.crudifier.IObjectRenderer;
 
 public class CrudifierView<T> extends Panel implements IGenericComponent<T> {
 	private static final long serialVersionUID = -151637566983702881L;
-	
+
 	private CrudifierEntitySettings entitySettings = new CrudifierEntitySettings();
 	private Map<Class<?>, IObjectRenderer<?>> renderers = new HashMap<Class<?>, IObjectRenderer<?>>();
 
 	public CrudifierView(String id, IModel<T> model) {
 		super(id, model);
-		
+
 		add(new Label("legend", new StringResourceModel("legend", this, getModel(), "Unknown")){
 			private static final long serialVersionUID = -7854751811138463187L;
 
 			@Override
 			protected void onConfigure() {
 				super.onConfigure();
-				
+
 				setVisible(!getDefaultModelObjectAsString().isEmpty());
 			}
 		});
-		
-		
+
+
 
 	}
-	
+	@Override
+	protected void onConfigure() {
+		RepeatingView view = new RepeatingView("control");
+		if(null != getModelObject()) {
+			for(final String property : getPropertiesByOrder(getModelObject().getClass())){
+				WebMarkupContainer control = new WebMarkupContainer(view.newChildId());
+				view.addOrReplace(control);
+
+				control.addOrReplace(new Label("label", new StringResourceModel("controls."+property+".label", this, getModel(), property)));
+				control.addOrReplace(new LabelProperty("input", new PropertyModel<Object>(getModel(), property), renderers) {
+					private static final long serialVersionUID = 8561120757563362569L;
+
+					@Override
+					protected String getResourceString(String key, String defaultValue) {
+						return getLocalizer().getStringIgnoreSettings("controls."+property+"."+key, CrudifierView.this, null, defaultValue);
+					}
+				});
+			}
+		}
+		addOrReplace(view);
+	}
+
 	@Override
 	protected void onInitialize() {
 		super.onInitialize();
-		
-		RepeatingView view = new RepeatingView("control");
-		for(final String property : getPropertiesByOrder(getModelObject().getClass())){
-			WebMarkupContainer control = new WebMarkupContainer(view.newChildId());
-			view.add(control);
-			
-			control.add(new Label("label", new StringResourceModel("controls."+property+".label", this, getModel(), property)));
-			control.add(new LabelProperty("input", new PropertyModel<Object>(getModel(), property), renderers) {
-				private static final long serialVersionUID = 8561120757563362569L;
-
-				@Override
-				protected String getResourceString(String key, String defaultValue) {
-					return getLocalizer().getStringIgnoreSettings("controls."+property+"."+key, CrudifierView.this, null, defaultValue);
-				}
-			});
-		}
-		add(view);
-		
 	}
 
 	private Set<String> getPropertiesByOrder(Class<?> modelClass) {
 		Set<String> properties = new LinkedHashSet<String>();
-		
+
 		for(String property : entitySettings.getOrderOfFields()){
 			if(!entitySettings.getHiddenFields().contains(property))
 				properties.add(property);
 		}
 		for(PropertyDescriptor descriptor : PropertyUtils.getPropertyDescriptors(modelClass)){
 			if(!entitySettings.getHiddenFields().contains(descriptor.getName()) &&
-			   !properties.contains(descriptor.getName()) &&
-			   !descriptor.getName().equals("class"))
+					!properties.contains(descriptor.getName()) &&
+					!descriptor.getName().equals("class"))
 				properties.add(descriptor.getName());
 		}
-		
+
 		return properties;
 	}
 
@@ -116,6 +119,6 @@ public class CrudifierView<T> extends Panel implements IGenericComponent<T> {
 	public void setRenderers(Map<Class<?>, IObjectRenderer<?>> renderers) {
 		this.renderers = renderers;
 	}
-	
-	
+
+
 }
