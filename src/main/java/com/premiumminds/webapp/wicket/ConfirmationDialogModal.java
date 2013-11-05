a8cecb01bf907d1c25d1df8d39000efcb428bae3
Filipe Roque <froque@premium-minds.com>
2013-11-05 19:01:01 +0000

    alterações à Modal de confirmação para as labels dos botões

--- src/main/java/com/premiumminds/webapp/wicket/ConfirmationDialogModal.java
+++ src/main/java/com/premiumminds/webapp/wicket/ConfirmationDialogModal.java
@@ -5,6 +5,7 @@ import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.util.io.IClusterable;
@@ -55,15 +56,16 @@ public class ConfirmationDialogModal extends ModalWindow{
 					ConfirmationDialogModal.this.close(target);
 					confirmed(target, Result.CANCELED);
 				}
-			});
-			Component yesButton = new AjaxFallbackLink<Void>("yesButton"){
+			}.add(new Label("noLbl", "Cancelar")));
+			WebMarkupContainer yesButton = new AjaxFallbackLink<Void>("yesButton"){
 				@Override
 				public void onClick(AjaxRequestTarget target) {
 					ConfirmationDialogModal.this.close(target);
 					confirmed(target, Result.OK);
 				}
 			}; 
-			yesButton.add(AttributeModifier.replace("value", verb));
+			yesButton.add(new Label("okLbl", verb));
+		
 			add(yesButton);
 		}
 	}
