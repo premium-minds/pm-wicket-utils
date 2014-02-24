0648d7f1a7707857387b7d6d2ee0397eceb8980d
Filipe Roque <froque@premium-minds.com>
2014-02-24 18:04:49 +0000

    alterado FormatLabel para que seja mais fácil usar nos testes
    alterada versão do java para 1.6

--- src/main/java/com/premiumminds/webapp/wicket/FormatLabel.java
+++ src/main/java/com/premiumminds/webapp/wicket/FormatLabel.java
@@ -1,12 +1,13 @@
 package com.premiumminds.webapp.wicket;
 
 import java.text.MessageFormat;
+import java.util.Locale;
 
+import org.apache.commons.beanutils.ConversionException;
 import org.apache.wicket.IGenericComponent;
-import org.apache.wicket.markup.ComponentTag;
-import org.apache.wicket.markup.MarkupStream;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.util.convert.IConverter;
 
 public class FormatLabel<T> extends Label implements IGenericComponent<T> {
 	private static final long serialVersionUID = 9039206115545313591L;
@@ -18,16 +19,33 @@ public class FormatLabel<T> extends Label implements IGenericComponent<T> {
 	public FormatLabel(String id){
 		super(id);
 	}
-
+	
 	@Override
-	public void onComponentTagBody(MarkupStream markupStream, ComponentTag openTag) {
-		if(getDefaultModel()==null) throw new RuntimeException("Model can't be null on label "+getPageRelativePath());
-		String format = getString("format", getDefaultModel());
-		MessageFormat messageFormat = new MessageFormat(format, getSession().getLocale());
-		
-		replaceComponentTagBody(markupStream, openTag, messageFormat.format(new Object[]{getDefaultModelObject()}));
+	public <C> IConverter<C> getConverter(Class<C> type) {
+		return new CustomConverter<C>();
 	}
 
+	private class CustomConverter<C> implements IConverter<C>{
+
+		private static final long serialVersionUID = 6258191476048316073L;
+
+		@Override
+		public C convertToObject(String value, Locale locale) throws ConversionException {
+			throw new ConversionException(value);
+		}
+
+		@Override
+		public String convertToString(C value, Locale locale) {
+			if(getDefaultModel()==null){
+				throw new RuntimeException("Model can't be null on label "+getPageRelativePath());
+			}
+			String format = getString("format", getDefaultModel());
+			MessageFormat messageFormat = new MessageFormat(format, getSession().getLocale());
+			
+			return messageFormat.format(new Object[]{getDefaultModelObject()});
+		}
+	}
+	
 	@SuppressWarnings("unchecked")
 	public IModel<T> getModel() {
 		return (IModel<T>) getDefaultModel();
