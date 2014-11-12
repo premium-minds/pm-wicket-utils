d407249bcc5b18f5667e0b682e0e2185f212e94f
André Camilo <andre.camilo@premium-minds.com>
2014-11-12 16:32:22 +0000

    map view with insertion order + tests

--- src/main/java/com/premiumminds/webapp/wicket/repeaters/MapItem.java
+++ src/main/java/com/premiumminds/webapp/wicket/repeaters/MapItem.java
@@ -1,39 +1,61 @@
+/**
+ * Copyright (C) 2014 Premium Minds.
+ *
+ * This file is part of pm-wicket-utils.
+ *
+ * pm-wicket-utils is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * pm-wicket-utils is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with pm-wicket-utils. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.premiumminds.webapp.wicket.repeaters;
-
-import java.util.Map;
-import java.util.Map.Entry;
 
 import org.apache.wicket.IGenericComponent;
 import org.apache.wicket.markup.html.list.AbstractItem;
 import org.apache.wicket.model.IModel;
 
-public class MapItem<K, V> extends AbstractItem implements IGenericComponent<Map.Entry<K, V>> {
+public class MapItem<K extends Number, V> extends AbstractItem implements IGenericComponent<V> {
 	private static final long serialVersionUID = 1613540034820483520L;
 
-	public MapItem(String id, IModel<Map.Entry<K, V>> model) {
-		super(id, model);
+	private K key;
+	
+	public MapItem(K key, IModel<V> model) {
+		super(String.valueOf(key), model);
+		this.key = key;
 	}
 
+	public K getKey(){
+		return key;
+	}
+	
 	@SuppressWarnings("unchecked")
 	@Override
-	public IModel<Entry<K, V>> getModel() {
-		return (IModel<Entry<K, V>>) getDefaultModel();
+	public IModel<V> getModel() {
+		return (IModel<V>) getDefaultModel();
 	}
 
 	@Override
-	public void setModel(IModel<Entry<K, V>> model) {
+	public void setModel(IModel<V> model) {
 		throw new UnsupportedOperationException("can't change model");
 	}
 
 	@Override
-	public void setModelObject(Entry<K, V> object) {
+	public void setModelObject(V object) {
 		throw new UnsupportedOperationException("can't change model");
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public Entry<K, V> getModelObject() {
-		return (Entry<K, V>) getDefaultModelObject();
+	public V getModelObject() {
+		return (V) getDefaultModelObject();
 	}
 
 }
--- src/main/java/com/premiumminds/webapp/wicket/repeaters/MapView.java
+++ src/main/java/com/premiumminds/webapp/wicket/repeaters/MapView.java
@@ -1,24 +1,43 @@
+/**
+ * Copyright (C) 2014 Premium Minds.
+ *
+ * This file is part of pm-wicket-utils.
+ *
+ * pm-wicket-utils is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * pm-wicket-utils is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with pm-wicket-utils. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.premiumminds.webapp.wicket.repeaters;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.apache.wicket.Component;
 import org.apache.wicket.IGenericComponent;
-import org.apache.wicket.markup.html.list.AbstractItem;
 import org.apache.wicket.markup.repeater.AbstractRepeater;
-import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 
 public abstract class MapView<K extends Number,V extends Serializable> extends AbstractRepeater implements IGenericComponent<Map<K, V>> {
 	private static final long serialVersionUID = -3771292136946484795L;
 	
-	private Map<K, Component> components = new HashMap<K, Component>();
+	private Map<K, MapItem<K, V>> mapComponents = new HashMap<K, MapItem<K, V>>();
+	private List<MapItem<K, V>> components = new ArrayList<MapItem<K, V>>();
+	
+	private boolean insertionOrderInverted = false;
 
 	public MapView(String id, IModel<? extends Map<K, V>> model) {
 		super(id, model);
@@ -29,35 +48,47 @@ public abstract class MapView<K extends Number,V extends Serializable> extends A
 	}
 	
 	@Override
+	protected void onInitialize() {
+		super.onInitialize();
+		boolean temp = insertionOrderInverted;
+		insertionOrderInverted = false;
+		populate();
+		insertionOrderInverted = temp;
+	}
+	
+	@Override
 	protected Iterator<? extends Component> renderIterator() {
-		return components.values().iterator();
+		return components.iterator();
 	}
 
 	@Override
 	protected void onPopulate() {
 		Map<K, V> mapFromModel = getModelObject();
 
-		Iterator<Map.Entry<K, Component>> iterator = components.entrySet().iterator();
+		Iterator<MapItem<K, V>> iterator = components.iterator();
 		while(iterator.hasNext()){
-			Map.Entry<K, Component> entry = iterator.next();
-			if(!mapFromModel.containsKey(entry.getKey())){
-				remove(entry.getValue());
+			MapItem<K, V> component = iterator.next();
+			if(!mapFromModel.containsKey(component.getKey())){
+				remove(component);
 				iterator.remove();
+				mapComponents.remove(component.getKey());
 			}
 		}
 		
 		for(Map.Entry<K, V> entry : mapFromModel.entrySet()){
-			if(!components.containsKey(entry.getKey())){
-				AbstractItem newItem = newItem(new ReadOnlyModel(entry));
-				components.put(entry.getKey(), newItem);
+			if(!mapComponents.containsKey(entry.getKey())){
+				MapItem<K, V> newItem = newItem(entry.getKey(), Model.of(entry.getValue()));
+				mapComponents.put(entry.getKey(), newItem);
+				if(insertionOrderInverted) components.add(0, newItem);
+				else components.add(newItem);
 				add(newItem);
 			}
 		}
 	}
 
-	protected MapItem<K, V> newItem(IModel<Map.Entry<K, V>> itemModel)
+	protected MapItem<K, V> newItem(K key, IModel<V> itemModel)
 	{
-		MapItem<K, V> abstractItem = new MapItem<K, V>(itemModel.getObject().getKey().toString(), itemModel);
+		MapItem<K, V> abstractItem = new MapItem<K, V>(key, itemModel);
 		populateItem(abstractItem);
 		return abstractItem;
 	}
@@ -67,6 +98,14 @@ public abstract class MapView<K extends Number,V extends Serializable> extends A
 	}
 	
 	protected abstract void populateItem(MapItem<K, V> item);
+	
+	public void populate(){
+		onPopulate();
+	}
+	
+	public MapItem<K, V> getMapComponent(K key){
+		return mapComponents.get(key);
+	}
 
 	@SuppressWarnings("unchecked")
 	@Override
@@ -89,35 +128,12 @@ public abstract class MapView<K extends Number,V extends Serializable> extends A
 	public Map<K, V> getModelObject() {
 		return (Map<K, V>) getDefaultModelObject();
 	}
-	
-	private class ReadOnlyModel extends AbstractReadOnlyModel<Map.Entry<K,V>> implements Serializable {
-		private static final long serialVersionUID = -2856441578148609149L;
-		
-		private Pair<K,V> entry;
-		
-		public ReadOnlyModel(Map.Entry<K,V> entry) {
-			this.entry = Pair.of(entry.getKey(), entry.getValue());
-		}
 
-		@Override
-		public Entry<K, V> getObject() {
-			return new Map.Entry<K, V>() {
+	public boolean isInsertionOrderInverted() {
+		return insertionOrderInverted;
+	}
 
-				@Override
-				public K getKey() {
-					return entry.getLeft();
-				}
-
-				@Override
-				public V getValue() {
-					return entry.getRight();
-				}
-
-				@Override
-				public V setValue(V value) {
-					throw new UnsupportedOperationException("can't call setValue");
-				}
-			};
-		}
+	public void setInsertionOrderInverted(boolean insertionOrderInverted) {
+		this.insertionOrderInverted = insertionOrderInverted;
 	}
 }
