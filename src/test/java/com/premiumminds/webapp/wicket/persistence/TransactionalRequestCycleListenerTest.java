e281e05afe2fb6bcb381fb2af22b731a0bdde3f2
Filipe Roque <froque@premium-minds.com>
2019-12-10 10:28:28 +0000

    added unit test for TransactionalRequestCycleListener

---
+++ src/test/java/com/premiumminds/webapp/wicket/persistence/TransactionalRequestCycleListenerTest.java
@@ -0,0 +1,60 @@
+package com.premiumminds.webapp.wicket.persistence;
+
+import com.premiumminds.persistence.PersistenceTransaction;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.util.tester.WicketTester;
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+public class TransactionalRequestCycleListenerTest {
+
+    @Test
+    public void testSuccess(){
+
+        PersistenceTransaction transaction = EasyMock.mock(PersistenceTransaction.class);
+
+        // 2 times because of a redirect
+        transaction.start();
+        EasyMock.expectLastCall().times(2);
+        transaction.end();
+        EasyMock.expectLastCall().times(2);
+
+        EasyMock.replay(transaction);
+
+        TransactionalRequestCycleListener listener = new TransactionalRequestCycleListener(transaction);
+
+        WicketTester tester = new WicketTester();
+        tester.getApplication().getRequestCycleListeners().add(listener);
+
+        tester.startComponentInPage(new Label("text", new Model<>()));
+
+        EasyMock.verify(transaction);
+    }
+
+    @Test
+    public void testOnExceptionRollback(){
+
+        PersistenceTransaction transaction = EasyMock.mock(PersistenceTransaction.class);
+
+        transaction.start();
+        transaction.setRollbackOnly();
+        transaction.end();
+
+        EasyMock.replay(transaction);
+
+        TransactionalRequestCycleListener listener = new TransactionalRequestCycleListener(transaction);
+
+        WicketTester tester = new WicketTester();
+        tester.getApplication().getRequestCycleListeners().add(listener);
+
+        try{
+            tester.startComponentInPage(new TextField<>("text", new Model<>()));
+        }catch (Exception e){
+
+        }
+
+        EasyMock.verify(transaction);
+    }
+}
